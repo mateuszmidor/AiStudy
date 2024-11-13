0c8e08664ee45ba63b53bb8a7addcd4a710eb1a9
Mateusz Midor <3demaniac@gmail.com>
2024-11-13 20:44:30 +0100

    finish vision_demo

--- ai_devs_3/vision_demo/main.go
+++ ai_devs_3/vision_demo/main.go
@@ -10,14 +10,27 @@ import (
 
 func main() {
 	start := time.Now()
-	// demo_ollama("llava:7b")
-	// demo_openai("gpt-4o-mini")
+	demo_ollama("llava:7b")
 	elapsed := time.Since(start)
+	fmt.Printf("Execution time: %s\n", elapsed)
+
+	fmt.Println()
+
+	start = time.Now()
+	demo_openai("gpt-4o-mini")
+	elapsed = time.Since(start)
 	fmt.Printf("Execution time: %s\n", elapsed)
 }
 
-func demo_ollama() {
-	result, err := ollama.Completion("describe what's on the picture", "Be eloquent", ollama.ImageFromFile("../avocado.png"), "llama3.2-vision:11b")
+// models tested on Asus TUF: Ryzen4800H 16 cores + GTX1660Ti 6GB VRAM + 16GB RAM
+// - llava:7b - 5sec (GPU)
+// - llama3.2-vision:11b - 18s (CPU, needs 8GB VRAM)
+// models tested on asus MacBook PRO 16-inch 2019: Intel i7 2.6GHz 6 cores + integrated graphics + 16GB RAM. All run on CPU
+// - llava:7b - 12sec
+// - llama3.2-vision:11b - 4m13s
+func demo_ollama(model string) {
+	fmt.Println("demo_ollama", model)
+	result, err := ollama.Completion("describe what's on the picture", "Be eloquent", ollama.ImageFromFile("../avocado.png"), model)
 	if err != nil {
 		fmt.Printf("ollama error: %+v", err)
 		return
@@ -26,7 +39,8 @@ func demo_ollama() {
 }
 
 func demo_openai(model string) {
-	result, err := openai.Completion("Read ALL text in red found in the picture", "You are OCR expert in reading Polish language", openai.ImageFromURL("https://assets-v2.circle.so/837mal5q2pf3xskhmfuybrh0uwnd"), model)
+	fmt.Println("demo_openai", model)
+	result, err := openai.Completion("describe what's on the picture", "Be eloquent", openai.ImageFromFile("../avocado.png"), model)
 	if err != nil {
 		fmt.Printf("openai error: %+v", err)
 		return
