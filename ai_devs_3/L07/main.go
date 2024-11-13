6d51a461bc1ed8394cc82f305ba25c0149658bb9
Mateusz Midor <3demaniac@gmail.com>
2024-11-13 21:51:30 +0100

    victorious implementation of L07

--- ai_devs_3/L07/main.go
+++ ai_devs_3/L07/main.go
@@ -4,22 +4,25 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/mateuszmidor/AiStudy/ai_devs_3/internal/ollama"
+	"github.com/mateuszmidor/AiStudy/ai_devs_3/internal/openai"
 )
 
-const system = "The polish city which map is attached in fragments has granaries and a fortress"
-const user = "Recognize the name of the city in Poland from the provided fragments of the city map. Note: one fragment belongs to a different city that doesn't interes us"
+const system = "You are an expert in maps and you know cities in Poland very well. Your task is to find out polish city from street names."
+const user = `
+1. Recognize the street names from attached map fragments. 
+2. Find out the name of the city in Poland. You are looking for polish city that has granaries and a fortress. 
+Note: one map fragment belongs to a different city that doesn't interests us.
+`
 
 func main() {
-	mapFragments := []string{"mapa1.jpg", "mapa2.jpg", "mapa3.jpg", "mapa4.jpg"}
-	images := make([]string, len(mapFragments))
-	for i, filename := range mapFragments {
-		images[i] = ollama.ImageFromFile(filename)
+	filenames := []string{"mapa1.jpg", "mapa2.jpg", "mapa3.jpg", "mapa4.jpg"}
+	images := make([]string, len(filenames))
+	for i, filename := range filenames {
+		images[i] = openai.ImageFromFile(filename)
 	}
-	resp, err := ollama.Completion(user, system, images, "llava:7b")
+	resp, err := openai.Completion(user, system, images, "gpt-4o")
 	if err != nil {
 		log.Fatalf("Error from openai: %+v", err)
 	}
 	fmt.Println(resp)
-	fmt.Println()
 }
