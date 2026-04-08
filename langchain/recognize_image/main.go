12518ca3a5fc761111d79ae1d1dc926655f4e0b1
Mateusz Midor <3demaniac@gmail.com>
2026-04-08 16:10:45 +0200

    add token usage info

--- langchain/recognize_image/main.go
+++ langchain/recognize_image/main.go
@@ -49,5 +49,29 @@ func main() {
 	}
 
 	// 4. prints out description of the image
+	fmt.Println("Image Description:")
 	fmt.Println(response.Choices[0].Content)
+	fmt.Println()
+
+	// 5. prints out token usage
+	if len(response.Choices) > 0 && response.Choices[0].GenerationInfo != nil {
+		if promptTokens, ok := response.Choices[0].GenerationInfo["PromptTokens"].(int); ok {
+			if completionTokens, ok := response.Choices[0].GenerationInfo["CompletionTokens"].(int); ok {
+				if totalTokens, ok := response.Choices[0].GenerationInfo["TotalTokens"].(int); ok {
+					fmt.Printf("Token Usage: %d input tokens, %d output tokens, %d total tokens\n",
+						promptTokens,
+						completionTokens,
+						totalTokens)
+				} else {
+					fmt.Println("Token usage information not available")
+				}
+			} else {
+				fmt.Println("Token usage information not available")
+			}
+		} else {
+			fmt.Println("Token usage information not available")
+		}
+	} else {
+		fmt.Println("Token usage information not available")
+	}
 }
